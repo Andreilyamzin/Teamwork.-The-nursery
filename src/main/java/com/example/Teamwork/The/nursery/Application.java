af6ece7150de79b9a4569ca3bf83ccbdf4ed375a
Lyamzin <lyamzin01@mail.ru>
2024-06-17 22:03:42 +1000

    1

---
+++ src/main/java/com/example/Teamwork/The/nursery/Application.java
@@ -0,0 +1,13 @@
+package com.example.Teamwork.The.nursery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
